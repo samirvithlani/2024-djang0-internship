0d2612ba12b61ee3166d4058ec2221a1cbe8ba81
samirvithlani <samir.vithlani83955@gmail.com>
2024-02-06 10:10:51 +0530

    delete

--- pms/employee/urls.py
+++ pms/employee/urls.py
@@ -5,6 +5,8 @@ from .import views
 urlpatterns = [
     #localhost:8000/employee/create
     path("create/",views.create_employee),
-    path("list/",views.getall_employee)
+    path("list/",views.getall_employee),
+    #localhost:8000/employee/delete/1
+    path("delete/<int:id>/",views.delete_employee,name = "delete_employee"),
 ]
    
--- pms/employee/views.py
+++ pms/employee/views.py
@@ -23,3 +23,18 @@ def getall_employee(request):
     print(employees)
     context["employees"] = employees
     return render(request,"employee/employee_list.html",context)
+
+
+#delete from employee where id = 1
+
+
+def delete_employee(request,id):
+    print("id....",id)
+    
+    context = {} #empty dictionary
+    employee = Employee.objects.get(id=id)
+    x = employee.delete()
+    print("x--->",x)
+    #print(employee)
+    
+    return render(request,"employee/employee_list.html",context)
